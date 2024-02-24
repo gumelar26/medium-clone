386e1a73bbe75f9f46a0b45bf26e675729aaacc4
gumelar26 <gumelarmuhamad26@gmail.com>
2024-02-25 01:05:25 +0700

    init commit crud users with sqlmodel

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 
-from user import get_user
+from user import get_user, update_user as crud_update_user, delete_user as crud_delete_user
 from database.schemas import UserGet, UserUpdate
 
 app = FastAPI()
@@ -12,6 +12,14 @@ async def get_current_user():
     raise HTTPException(status_code=404, detail="User not found")
   return user
 
-# @app.put("/api/user", response_model=UserUpdate)
-# async def update_user():
-  
+@app.put("/api/user")
+async def update_user(
+  userSchema: UserUpdate
+):
+  crud_update_user(username="andiuntung", userSchema=userSchema)
+  return {"status": 200}
+
+@app.delete("/api/user")
+async def delete_user():
+  crud_delete_user(username="andiuntung")
+  return {"status": 200}
--- app/user.py
+++ app/user.py
@@ -1,6 +1,8 @@
 from sqlmodel import Session, select
+from fastapi import HTTPException
 
 from database.model import engine, User
+from database.schemas import UserUpdate
 
 def get_user(username : str):
     with Session(engine) as session:
@@ -10,13 +12,21 @@ def get_user(username : str):
         print(users)
         return users
 
-def update_user(username: str):
+def update_user(username: str, userSchema: UserUpdate):
     with Session(engine) as session:
         users = session.exec(
             select(User).where(User.username == username)
         ).one()
         
-        users.password= "secret"
+        if users is None:
+            raise HTTPException(status_code=404, detail="Can't update data")
+        
+        users.username = userSchema.username
+        users.email = userSchema.email
+        users.token = userSchema.token
+        users.modified_at = userSchema.modified_at
+        users.status = userSchema.status
+        
         session.add(users)
         session.commit()
         session.refresh(users)
